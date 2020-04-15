28dbf992217157ec35bf0c95f864068f2b13f239
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-04-15 17:53:58 +0900

    【ポートフォリオ実装】20200415

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -19,78 +19,128 @@
     <a
       href="#"
       class="skillCategoriesFrontend"
-      @click="setCurrentChart('bbbb')"
+      @click="setCurrentChart('Frontend')"
     >
       Frontend
     </a>
     <a
-      href=""
+      href="#"
       class="skillCategoriesBackend"
+      @click="setCurrentChart('Backend')"
     >
       Backend
     </a>
     <a
-      href=""
+      href="#"
       class="skillCategoriesDevOps"
+      @click="setCurrentChart('DevOps')"
     >
       DevOps
     </a>
     <br>
-    <div class="skillListFrontend">
-      <span class="skillFrontend">HTML</span>
-      <span class="skillFrontend">CSS</span>
-      <span class="skillFrontend">JavaScript</span>
-      <span class="skillFrontend">SCSS</span>
-      <span class="skillFrontend">Vue</span>
-    </div>
-    <div class="skillListBackend">
-      <span class="skillBackend">Java</span>
-      <span class="skillBackend">Ruby</span>
-      <span class="skillBackend">RubyOnRails</span>
-      <span class="skillBackend">MySQL</span>
-    </div>
-    <div class="skillListDevOps">
-      <span class="skillDevOps">Linux</span>
-      <span class="skillDevOps">Node</span>
-      <span class="skillDevOps">Git</span>
-      <span class="skillDevOps">GitHub</span>
-      <span class="skillDevOps">Firebase</span>
-    </div>
-    <div class="skillGraph">
-      <div id="SkilChartFrontend">
-        <SkilChartFrontend />
+    <div>
+      <div>
+        <div
+          class="offFrontend"
+          :class="{ onFrontend: isFrontendActive }"
+        >
+          <SkillFrontend />
+        </div>
+        <div
+          class="offBackend"
+          :class="{ onBackend: isBackActive }"
+        >
+          <lu class="skillListBackend">
+            <li>Java</li>
+            <li>Ruby</li>
+            <li>RubyOnRails</li>
+            <li>MySQL</li>
+          </lu>
+        </div>
+        <div
+          class="offDevOps"
+          :class="{ onDevOps: isDevOpsActive }"
+        >
+          <div id="DevOps">
+            <div class="skillListDevOps">
+              <span class="skillDevOps">Linux</span>
+              <span class="skillDevOps">Node</span>
+              <span class="skillDevOps">Git</span>
+              <span class="skillDevOps">GitHub</span>
+              <span class="skillDevOps">Firebase</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div id="SkilChartBackend">
-        <SkilChartBackend />
-      </div>
-      <div id="SkilChartDevOps">
-        <SkilChartDevOps />
+      <div class="skillGraph">
+        <div v-if="isFrontActive">
+          <SkillChartFrontend />
+        </div>
+        <div v-if="isBackActive">
+          <SkillChartBackend />
+        </div>
+        <div v-if="isDevOpsActive">
+          <SkillChartDevOps />
+        </div>
       </div>
     </div><!--試し-->
   </div>
 </template>
 
 <script>
-import SkilChartFrontend from './SkilChartFrontend.vue'
-import SkilChartBackend from './SkilChartBackend.vue'
-import SkilChartDevOps from './SkilChartDevOps.vue'
+import SkillChartFrontend from './SkillChart/SkillChartFrontend.vue'
+import SkillChartBackend from './SkillChart/SkillChartBackend.vue'
+import SkillChartDevOps from './SkillChart/SkillChartDevOps.vue'
+import SkillFrontend from './SkillList/SkillFrontend.vue'
+
 
 export default {
   name: "Skill",
   components: {
-    SkilChartFrontend,
-    SkilChartBackend,
-    SkilChartDevOps
+    SkillChartFrontend,
+    SkillChartBackend,
+    SkillChartDevOps,
+    SkillFrontend,
   },
-  data: function () {
+  data () {
     return {
-      currentChart: 'front'
+      currentChart: 'Frontend'
+    }
+},
+  computed: {
+    isFrontActive() {
+      return this.currentChart=='Frontend';
+    },
+    isBackActive() {
+      return this.currentChart=='Backend';
+    },
+    isDevOpsActive() {
+      return this.currentChart=='DevOps';
     }
   },
   methods: {
     setCurrentChart(chart) {
-      console.log(chart);
       this.currentChart = chart;
+      console.log(this.currentChart);
+      console.log(this.currentChart=='Frontend');
+      console.log(this.currentChart=='Backend');
+      console.log(this.currentChart=='DevOps');
+
+/*いらなかったswich            switch (this.currentChart) {
+              case "Frontend":
+        console.log(`${this.currentChart}Frontendye!`);
+        return this.currentChart = 'chart';
+        break;
+          case "Backend":
+        console.log(`${this.currentChart}Backendye!`);
+                return this.currentChart = 'chart';
+
+        break;
+
+              case "DevOps":
+        console.log(`${this.currentChart}DevOpsye!`);
+        break;
+      }*/
     }
   }
 }
@@ -149,24 +199,23 @@ export default {
   opacity: 0.75;
 }
 
-.skillListFrontend {
+.offFrontend {
   opacity: 0.85;
   display: inline-block;
   height: 30px;
   border-radius: 20%;
 }
 
-.skillListBackend {
-  color: #0f8839;
-  opacity: 0.25;
+/* .skillListBackend {
+  color: rgba(15, 136, 57, 0.25);
   background: #0f8839;
   display: inline-block;
   height: 30px;
   border-radius: 20%;
-}
+} */
 
 .skillListDevOps {
-  color: #571083;
+  color: rgba(87, 16, 131, 0.25);
   background: #571083;
   opacity: 0.25;
   display: inline-block;
@@ -175,9 +224,19 @@ export default {
 }
 
 .skillFrontend {
-  background: rgba(241, 6, 6, 0.278);
+  background: rgba(241, 6, 6, 0.25);
   margin: 10px;
-  color: rgba(241, 6, 6, 0.678);
+  color: rgba(241, 6, 6, 0.75);
+}
+
+.skillListBackend li {
+  display: inline;
+  color: rgba(15, 136, 57, 0.25);
+  padding: 5px 10px 5px 10px;
+  font-weight: bold;
+  background: rgba(15, 136, 57, 0.25);
+  box-shadow: 0 0 8px gray;
+  margin: 5px;
 }
 
 #SkilChartFrontend {
