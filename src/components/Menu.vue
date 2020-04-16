16c2e5e4948614bd5b1a93d178aa2a18b21f4577
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-04-16 18:29:51 +0900

    【ポートフォリオ実装】20200416-3_メニュー修正

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -20,6 +20,7 @@
               v-smooth-scroll
               class="drawerMenuItemLink"
               href="#Main"
+              @click="toggle()"
             >Main</a>
           </li>
           <li class="drawerMenuItem">
@@ -27,6 +28,7 @@
               v-smooth-scroll
               class="drawerMenuItemLink"
               href="#About"
+              @click="toggle()"
             >About</a>
           </li>
           <li class="drawerMenuItem">
@@ -34,6 +36,7 @@
               v-smooth-scroll
               class="drawerMenuItemLink"
               href="#Skill"
+              @click="toggle()"
             >Skill</a>
           </li>
           <li class="drawerMenuItem">
@@ -41,25 +44,12 @@
               v-smooth-scroll
               class="drawerMenuItemLink"
               href="#Vision"
+              @click="toggle()"
             >Vision</a>
           </li>
         </ul>
       </div>
     </Drawer>
-    <!--追加したドロワー-
-  <div id="drawer">
-    <div>
-      <button @click="openDrawerMenu('true')">ボタン</button>
-    </div>
-    <transition name="right">
-      <div v-if="drawerFlg" class="drawer-menu-wrapper">
-        <div class="drawer-menu">
-          <button @click="openDrawerMenu('false')">ボタン</button>
-          ここにメニューの内容を書いていく
-        </div>
-      </div>
-  </transition>
-  </div>追加したドロワー-->
   </div>
 </template>
 
@@ -82,22 +72,6 @@ export default {
 
     }
   }
-  /*追加したドロワー
-    data() {
-    return {
-      drawerFlg: false
-    };
-  },
-  computed: {
-
-    this.drawerFlg
-  },
-  methods: {
-    openDrawerMenu(newdrawer) {
-      this.drawerFlg = newdrawer;
-    }
-  }
-  追加したドロワー*/
 };
 </script>
 <!--ドロワーの装飾
@@ -118,18 +92,6 @@ export default {
   width: 300px;
 }
 
-/* 元からあったドロワーボタンの装飾（一応残す）
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding-top: 150px;
-  margin: 0 auto;
-  width: 800px;
-}
-*/
 #headerSection {
   background: #f3f3f3;
   width: 100%;
@@ -143,32 +105,6 @@ export default {
   height: 20px;
   padding: 0 0 0 10px;
 }
-
-/* 新たに追加したドロワー
-左から出したい場合
-.left-enter-active, .left-leave-active {
-  transform: translate(0px, 0px);
-  transition: transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms;
-}
-.left-enter, .left-leave-to {
-  transform: translateX(-100vw) translateX(0px);
-}
-
-以下、メニューの形に合わせて良い具合に変更してください
-.drawer-menu-wrapper {
-  position: absolute;
-  z-index: 10;
-  top: 0;
-  right: 0; 右に出す場合
-  left: 0 ;左に出す場合
-  width: 50%;
-  height: 100%;
-  background-color: white;
-}
-.drawer-menu {
-  padding: 24px;
-}
-新たに追加したドロワー */
 
 .drawerSection {
   color: #f3f3f3;
