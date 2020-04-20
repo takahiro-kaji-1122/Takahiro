8b0d936d1a56af6be0e7c506d9c9176d5e393e3a
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2020-04-20 17:13:27 +0900

    【API接続】20200420

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/reset.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+import store from './store'
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
+
+
+
+
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex) //vuexが読み込まれている
+
+// storeをエクスポート
+export default new Vuex.Store({ //ここに実装を書く
+  state: {
+
+  },
+
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
